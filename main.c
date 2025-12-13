bab9cf9d9b1444f4e0ace101685bca939242dd10
eduar-amaral <eduardo.a.bassa97@gmail.com>
2025-12-13 02:52:43 +0000

    feat:  create interactive test for get_next_line
    - Add modular test functions (test_normal_file, test_empty_file, test_stdin)
    - Implement wait_for_enter() for controlled test progression
    - All functions under 25 lines
    - Proper error handling without aborting execution

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: eamaral- <eamaral-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/11 21:23:38 by eamaral-          #+#    #+#             */
-/*   Updated: 2025/12/11 22:04:40 by eamaral-         ###   ########.fr       */
+/*   Updated: 2025/12/13 02:46:01 by eamaral-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,45 +14,103 @@
 #include <fcntl.h>
 #include <stdio.h>
 
-int main(void)
+static void	wait_for_enter(void)
+{
+	printf("\n[Press ENTER to continue...]");
+	fflush(stdout);
+	while (getchar() != '\n')
+		;
+}
+
+static void	test_normal_file(void)
 {
 	int		fd;
 	char	*line;
 	int		i;
 
-	// test1 - Normal file
 	fd = open("test.txt", O_RDONLY);
 	if (fd < 0)
 	{
-		printf("Error, can't open file.\n");
-		return (1);
+		printf("Error: can't open test.txt (skipped)\n");
+		return ;
 	}
 	i = 1;
-	printf("=== Lendo test.txt ===\n");
-	while ((line = get_next_line(fd)))
+	printf("\n+---------------------------+\n");
+	printf("| Reading test.txt          |\n");
+	printf("+---------------------------+\n");
+	while (1)
 	{
-		printf("[%d]:  %s", i, line);
+		line = get_next_line(fd);
+		if (!line)
+			break ;
+		printf("\n[%d]:  %s\n", i, line);
 		free(line);
 		i++;
 	}
 	close(fd);
-	// Test 2 - Empty file
+}
+
+static void	test_empty_file(void)
+{
+	int		fd;
+	char	*line;
+
 	fd = open("empty.txt", O_RDONLY);
-	if (fd >= 0)
+	if (fd < 0)
 	{
-		printf("\n=== Reading empty.txt ===\n");
-		line = get_next_line(fd);
-		printf("Resultado: %s\n", line ?  line : "NULL (correto!)");
-		if (line)
-			free(line);
-		close(fd);
+		printf("Error: Can't open empty.txt (skipped)\n");
+		return ;
 	}
-	// Teste 3: Stdin (teste manual)
-	printf("\n=== Write anything (Ctrl+D to finish) ===\n");
-	while ((line = get_next_line(0)))
+	printf("\n+---------------------------+\n");
+	printf("| Reading empty.txt         |\n");
+	printf("+---------------------------+\n");
+	line = get_next_line(fd);
+	if (!line)
+		printf("\nResult: File is empty (correct!)\n");
+	else
 	{
-		printf("you wrote: %s", line);
+		printf("Result: File should be empty but got: %s", line);
 		free(line);
 	}
+	close(fd);
+}
+
+static void	test_stdin(void)
+{
+	char	*line;
+	int		i;
+
+	printf("\n+----------------------------------------+\n");
+	printf("| Reading stdin (Ctrl+D to exit)         |\n");
+	printf("+--------------------------------------- +\n");
+	i = 1;
+	while (1)
+	{
+		printf("\n> ");
+		fflush(stdout);
+		line = get_next_line(0);
+		if (!line)
+		{
+			printf("\r\033[K");
+			break ;
+		}
+		printf("\n[%d]: %s", i, line);
+		i++;
+		free(line);
+	}
+}
+
+int	main(void)
+{
+	printf("+=====================================+\n");
+	printf("|     GET_NEXT_LINE TEST              |\n");
+	printf("+=====================================+\n");
+	wait_for_enter();
+	test_normal_file();
+	wait_for_enter();
+	test_empty_file();
+	wait_for_enter();
+	test_stdin();
+	printf("\nThank you for testing it!\n");
 	return (0);
 }
